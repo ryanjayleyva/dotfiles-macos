f72db2da10e9480fe34b82c89de5704f523ce4ea
RJ Leyva <ryanjayleyva@proton.me>
2025-09-15 02:41:09 -0400

    chore(nvim): set up lazy.nvim with defaults, plugin import, and performance

---
+++ .config/nvim/lua/core/lazy.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+M.setup = function()
+  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+  if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable',
+      lazypath,
+    }
+  end
+
+  vim.opt.rtp:prepend(lazypath)
+
+  require('lazy').setup({
+    { import = 'plugins' },
+  }, {
+    defaults = {
+      lazy = true,
+      version = false,
+    },
+
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+
+    change_detection = {
+      enabled = true,
+      notify = false,
+    },
+
+    performance = {
+      rtp = {
+        disabled_plugins = {
+          -- 'matchit',
+          'matchparen',
+          -- 'netrwPlugin',
+          'tarPlugin',
+          'tohtml',
+          'tutor',
+          'gzip',
+          'zipPlugin',
+        },
+      },
+    },
+  })
+end
+
+return M
